6913a306ff829e96a248de07541641c6de8f1c1b
Daniel <danielerthaldapper2008@gmail.com>
2026-07-22 18:43:22 -0300

    build: initialize Spring Boot project

---
+++ src/main/java/io/github/danieldapper/pingado/PingadoApplication.java
@@ -0,0 +1,13 @@
+package io.github.danieldapper.pingado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingadoApplication.class, args);
+    }
+
+}
